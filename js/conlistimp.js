10ed60a52a289c128082da0219354f0fd1b70ddc
Izel <gpmeneses@gmail.com>
2013-10-01 13:22:52 -0500

    lectura de archivo VCF

--- js/conlistimp.js
+++ js/conlistimp.js
@@ -1,3 +1,27 @@
-$(function() {
-  $("div").text("Hello World!");
-});
+function loadContacts(fileName){
+	var storage = navigator.getDeviceStorage('sdcard');
+	var contactsHandle = storage.get(fileName);
+	
+	contactsHandle.onsuccess = function () {
+		var reader = FileReader();  		
+  		reader.readAsText(this.result,"utf-8");	
+  		reader.onloadend = function() {
+			var txt = this.result;
+			$("<p>" + txt  + "</p>").appendTo("#fileContent");
+      }
+  	}
+	
+	contactsHandle.onerror = function () {
+  			console.warn('Unable to get the file: ' + this.error);
+	}		
+}
+
+
+
+
+$(document).ready(function(){
+    $("#import").click(function(){    		    		
+        loadContacts('00002.vcf');
+    });
+ });   
+ 
